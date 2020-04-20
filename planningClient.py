fcf11c6678a3547b10c057f13c98121fcf864041
khushboo <khushii.me@gmail.com>
2020-04-20 09:06:57 +0200

    test hardcoding again

---
+++ planningClient.py
@@ -0,0 +1,124 @@
+import sys
+import argparse
+import memory
+import re
+from HardCoded import *
+
+
+def HandleError(message):
+    print(message,file=sys.stderr,flush=True)
+
+def Readlines(msg):
+    #add it when using sysin
+    #return msg.readline().rstrip()
+    #remove it when using sysin
+    return msg.pop(0).rstrip()
+    
+def ReadHeaders(messages) :
+    list_of_colors = ['blue','red','cyan','purple','green','orange','pink','grey','lightblue','brown']
+    line = Readlines(messages)
+    if line == '#domain' :
+        line = Readlines(messages)
+        if line != 'hospital' :
+            HandleError('Incorrect domain, it can only be hospital')
+        else:
+            ToServer('#Domain is '+line)
+    else :
+        HandleError('First line should be #domain') 
+    
+    line = Readlines(messages)
+    if line == '#levelname' :
+        line = Readlines(messages)
+        ToServer('#Level name is '+line)
+    else :
+        HandleError('Level name is missing') 
+    
+    line = Readlines(messages)
+    added = list()
+    if line == '#colors' :
+        color_dict = {}
+        while True :
+            line = Readlines(messages)
+            color_data = re.split(', |: |\s',line)
+            if color_data[0] in list_of_colors :
+                if color_data[0] in color_dict.keys() :
+                    HandleError('Color is repeated')
+                else :
+                    for box_or_agent in color_data :
+                        if box_or_agent in added :
+                            HandleError('Box or agent has already been specified')
+                        else :
+                            added.append(box_or_agent)
+                    color_dict[color_data[0]] = color_data[1:]
+            else :
+                if line[0] == '#' :
+                    break
+                else :
+                    HandleError('Unacceptable color')
+    else :
+        HandleError('Colors missing')
+    
+    if line == '#initial' :
+        line = Readlines(messages)
+        initial_state = list()
+        while line[0] == '+' :
+            initial_state.append(line)
+            line = Readlines(messages)
+    else :
+        HandleError('Initial state missing')
+             
+    if line == '#goal' :
+        line = Readlines(messages)
+        goal_state = list()
+        while line[0] == '+' :
+            goal_state.append(line)
+            line = Readlines(messages)
+    else :
+        HandleError('Goal state missing')
+             
+    if line != '#end' :
+        HandleError('End missing')    
+    
+    return color_dict,initial_state,goal_state
+ 
+                
+if __name__ == '__main__':    
+    # Set max memory usage allowed (soft limit).
+    parser = argparse.ArgumentParser(description='Client based on planning approach.')
+    parser.add_argument('--max-memory', metavar='<MB>', type=float, default=2048.0, help='The maximum memory usage allowed in MB (soft limit, default 2048).')
+    
+    strategy_group = parser.add_mutually_exclusive_group()
+    args = parser.parse_args()
+    memory.max_usage = args.max_memory
+    
+    # Run client.
+    try:
+        #add when using input from sysin
+        #server_messages = sys.stdin
+        #ToServer('PlanningClient')
+        #remove when using sysin
+        f=open('../SAExample.lvl','r')
+        server_messages = f.readlines()
+        f.close()
+        #remove until here
+        color_dict,initial_state,goal_state = ReadHeaders(server_messages) 
+            
+    except Exception as ex:
+        print('Error parsing level: {}.'.format(repr(ex)), file=sys.stderr, flush=True)
+        sys.exit(1)
+    
+    #below line is only for testing purpose                      
+    HardCodedForDefault()    
+    State.current_state = initial_state
+    
+    for agent,box_cells in current_plan.items() :
+        box = box_cells[0]
+        cells = box_cells[1]
+        agent.ExecutePlan(box,cells)
+        
+    
+#for c in cells :
+#    print(c)
+    
+#for c in current_plan[1] :
+#    print(c)       
